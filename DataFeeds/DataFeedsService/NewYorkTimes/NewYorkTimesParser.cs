40652270df34b00a5f7238df30b10f4f52d910c6
Meydan Dayan <t-meday@microsoft.com>
2015-07-27 17:15:31 +0300

    update NYT

--- DataFeeds/DataFeedsService/NewYorkTimes/NewYorkTimesParser.cs
+++ DataFeeds/DataFeedsService/NewYorkTimes/NewYorkTimesParser.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Web;
 using System.Xml.Schema;
+using DataFeedsService.Feeds;
 using Newtonsoft.Json.Linq;
 
 namespace DataFeedsService.faroo
 {
     public class NewYorkTimesParser : IDataFeedApi
     {
-        
+
+        private const string domain = "http://api.nytimes.com/"; 
         private const string urlTemplate =
-            "http://api.nytimes.com/svc/news/v3/content/all/{0}/{1}.xml?limit={2}&api-key=d3535b3bef9c3a82b2f63f763111b679:8:72573066";
+            "svc/news/v3/content/all/{0}/{1}.xml?limit={2}&api-key=d3535b3bef9c3a82b2f63f763111b679:8:72573066";
 
         private Dictionary<string, string> topicTranslator = new Dictionary<string, string>()
         {
@@ -22,12 +25,12 @@ namespace DataFeedsService.faroo
 
         public async Task<DataFeed[]> GetFeedsAsync(string topic, int maxResults, DateTime queryStartTime)
         {
-            DataFeed[] feeds = new DataFeed[]; 
+            List<DataFeed> feeds = new List<DataFeed>(); 
             string nytTopic = topicTranslator[topic];
             int periodInHours = (DateTime.Now - queryStartTime).Hours;
             string url = string.Format(urlTemplate, nytTopic, periodInHours, maxResults);
 
-            string response = null; //Michael's function
+            string response = (await ApiHandler.GetResponseAsync(domain,url)).Content.ToString();
             JObject jsonResponse = JObject.Parse(response);
 
             int resultsFeedNumber = (int) jsonResponse["num_results"];
@@ -36,16 +39,19 @@ namespace DataFeedsService.faroo
                 var result = jsonResponse["result"][i];
                 //result["multimedia"].Children()
 
-                
+
                 DataFeed feed = new DataFeed
                 {
-                    Link = new Url((string)result["url"]),
+                    Link = new Url((string) result["url"]),
                     Title = (string) result["title"],
-                    PublishTime = DateTime.Parse((string)result["published_date"]),
+                    PublishTime = DateTime.Parse((string) result["published_date"]),
                     Source = (string) result["source"],
                     Image = new Url((string) result[""])
-                }
+                };
+                feeds.Add(feed);
             }
+
+            return feeds.ToArray();
         }
     }
 }
