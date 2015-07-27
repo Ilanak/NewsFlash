62ea50c9604a8fe0181c51396238140cc68b0512
Michael Merjanov <mimer@microsoft.com>
2015-07-27 17:52:38 +0300

    update alchemy api

--- DataFeeds/DataFeedsService/Feeds/Alchemy.cs
+++ DataFeeds/DataFeedsService/Feeds/Alchemy.cs
@@ -8,14 +8,30 @@ namespace DataFeedsService.Feeds
     {
         DataFeed[] results = new DataFeed[0];
 
-        public Task<DataFeed[]> GetFeedsAsync(string topic, int maxResults, DateTime time)
+        public async Task<DataFeed[]> GetFeedsAsync(string topic, int maxResults, DateTime startTime)
         {
             DataFeed item = new DataFeed();
-            string ApiBaseUrl = "http://api.feedzilla.com/";
-            string requestParameters = "/v1/categories/26/subcategories/1303/articles.json";
-            Task<HttpResponseMessage> response = ApiHandler.GetResponseAsync(ApiBaseUrl, requestParameters);
 
 
+            string start = "";
+            string end = "";
+            if (startTime == null)
+            {
+                startTime=new DateTime();
+            }
+            start = startTime.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).ToString();
+            end = startTime.AddDays(-7).Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).ToString();
+
+            string ApiBaseUrl = "https://access.alchemyapi.com/calls/data/";
+            string apiKey = "7adc0995828840783576e9d10754cc326542d34d";
+            string returnValues = "enriched.url.title,enriched.url.url,enriched.url.publicationDate";
+            string subject = "finance";
+            string requestParameters =
+                String.Format(
+                    "/GetNews?apikey={0}&return={1}&start={2}&end={3}&q.enriched.url.taxonomy.taxonomy_.label={4}&count=25&outputMode=json",
+                    apiKey,returnValues,start,end,subject);
+            HttpResponseMessage response = await ApiHandler.GetResponseAsync(ApiBaseUrl, requestParameters);
+  
 
             return response;
         }
