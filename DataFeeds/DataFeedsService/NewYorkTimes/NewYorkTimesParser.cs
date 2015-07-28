ae7a16968ba0131d6d6ca63207b39ccf8e5cd5e8
Meydan Dayan <t-meday@microsoft.com>
2015-07-28 14:49:56 +0300

    NYTParser - return feeds with no image

--- DataFeeds/DataFeedsService/NewYorkTimes/NewYorkTimesParser.cs
+++ DataFeeds/DataFeedsService/NewYorkTimes/NewYorkTimesParser.cs
@@ -72,7 +72,6 @@ namespace DataFeedsService.NewYorkTimes
                 {
                     var result = jsonResponse["results"][i];
 
-                    JArray multimedaArr = (JArray) result["multimedia"];
                     feed = new DataFeed
                     {
                         Link = (string) result["url"],
@@ -80,7 +79,7 @@ namespace DataFeedsService.NewYorkTimes
                         PublishTime = DateTime.Parse((string) result["created_date"]),
                         Source = (string) result["source"],
                         Text = (string) result["abstract"],
-                        Image = GetUrlOfLargestImage(multimedaArr)
+                        Image = GetUrlOfLargestImage(result)
                     };
                 }
                 catch(Exception e)
@@ -93,31 +92,35 @@ namespace DataFeedsService.NewYorkTimes
             return feeds.ToArray();
         }
 
-        private string GetUrlOfLargestImage(JArray multimedaArr)
-        {
+        private string GetUrlOfLargestImage(JToken result)
+        { 
             string largestUrl = null;
             int largestSize = 0;
-            if (multimedaArr == null || multimedaArr.Count == 0)
+
+            try
             {
-                return null;
-            }
-
-            for (int i = 0; i < multimedaArr.Count; i++)
-            {
-                if ((string) multimedaArr[i]["type"] != "image")
+                JArray multimedaArr = (JArray)result["multimedia"];
+                if (multimedaArr == null || multimedaArr.Count == 0)
                 {
-                    continue;
+                    return null;
                 }
-
-                int currentSize = (int) multimedaArr[i]["height"] * (int) multimedaArr[i]["width"];
-                if (currentSize > largestSize)
+                
+                for (int i = 0; i < multimedaArr.Count; i++)
                 {
-                    largestSize = currentSize;
-                    largestUrl = (string)multimedaArr[i]["url"];
+                    if ((string) multimedaArr[i]["type"] != "image")
+                    {
+                        continue;
+                    }
+
+                    int currentSize = (int) multimedaArr[i]["height"]*(int) multimedaArr[i]["width"];
+                    if (currentSize > largestSize)
+                    {
+                        largestSize = currentSize;
+                        largestUrl = (string) multimedaArr[i]["url"];
+                    }
                 }
             }
-
-            if (largestUrl == null)
+            catch (Exception e)
             {
                 return null;
             }
