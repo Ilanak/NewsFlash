b184dee5dc84a78de79e47d5cea45c9bc48a47c0
middleeast\assafn <assafn@microsoft.com>
2015-07-28 13:07:52 +0300

    Integrate feed part 1

--- NewsFlashUI/NewsFlashUI/Main.aspx.cs
+++ NewsFlashUI/NewsFlashUI/Main.aspx.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace NewsFlashUI
 {
@@ -12,7 +15,18 @@ namespace NewsFlashUI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string[] filePaths = Directory.GetFiles(@"C:\GitHubRepo\NewsFlash\NewsFlashUI\NewsFlashUI\Content", "*.jpg");
+            // DataFeedsService.svc/GetFeeds?Topic=s1
+            var client = new RestClient("http://datafeeds.cloudapp.net/");
+
+            var request = new RestRequest("DataFeedsService.svc/GetFeeds", Method.GET);
+            request.AddParameter("Topic", "s1");
+
+            var result = client.Execute(request);
+
+
+            var feeds = JsonConvert.DeserializeObject(result.Content);
+
+            string[] filePaths = new string[0]; // = Directory.GetFiles(@"C:\GitHubRepo\NewsFlash\NewsFlashUI\NewsFlashUI\Content", "*.jpg");
             for (int i = 0; i < filePaths.Length; i++){
                 string fileName = Path.GetFileNameWithoutExtension(filePaths[i]);
                 string[] words = fileName.Split(new char[] {' '});
